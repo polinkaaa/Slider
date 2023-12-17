badf97fa1627381a8bee42b274326a9a9f8a3289
polinkaaa <polina_kond@mail.ru>
2023-12-17 22:05:59 +0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-let dots = document.querySelectorAll('.dots-item'),
-    dotsArea = document.querySelectorAll('.dots')[0],
+import 'normalize.css';
+import './styles/main.scss';
+
+const dots = document.querySelectorAll('.dots-item'),
+    dotsArea = document.querySelector('.dots'),
     slides = document.querySelectorAll('.slides-item'),
     prevBtn = document.getElementById('left-button'),
     nextBtn = document.getElementById('right-button'),
-    btn = document.getElementById('btn'),
-    slideIndex = 1;
+    btn = document.getElementById('btn');
+let slideIndex = 1;
 
 showSlides(slideIndex);
 
@@ -14,12 +17,15 @@ function showSlides (n) {
     } else if (n > slides.length){
         slideIndex = slides.length;
     }
-    for (let i = 0; i < slides.length; i++){
-        slides[i].style.display = 'none';
-    }
-    for (let i = 0; i < dots.length; i++){
-        dots[i].classList.remove('is-active');
-    }
+
+    slides.forEach(function (item) {
+        item.style.display = 'none';
+    })
+
+    dots.forEach(function (item) {
+        item.classList.remove('is-active');
+    })
+
     slides[slideIndex-1].style.display = 'block';
     slides[slideIndex-1].classList.add('checked');
     dots[slideIndex-1].classList.add('is-active');
@@ -28,19 +34,23 @@ function showSlides (n) {
 function plusSlides (n) {
     showSlides(slideIndex += n);
 }
+
 function currentSlide (n) {
     showSlides(slideIndex = n);
 }
+
 prevBtn.onclick = function () {
     plusSlides(-1);
     checkSlides();
 }
+
 nextBtn.onclick = function () {
     plusSlides(1);
     checkSlides();
 }
+
 dotsArea.onclick = function (e) {
-    for (let i = 0; i<dots.length +1; i++) {
+    for (let i = 0; i < dots.length + 1; i++) {
         if (e.target.classList.contains('dots-item') && e.target == dots[i - 1]) {
             currentSlide(i);
         }
@@ -50,8 +60,8 @@ dotsArea.onclick = function (e) {
 
 function checkSlides() {
     let allSlidesClicked = true;
-    slides.forEach(slidet => {
-        if (!slidet.classList.contains('checked')) {
+    slides.forEach(function (item) {
+        if (!item.classList.contains('checked')) {
             allSlidesClicked = false;
         }
     });
